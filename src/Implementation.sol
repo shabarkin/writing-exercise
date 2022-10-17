87379dc56ce658334e1dd367020aed16e6cdf0d5
Pavel Shabarkin <52198712+shabarkin@users.noreply.github.com>
2022-10-17 22:11:15 +0200

    Fix critical vulnerability
    Changed the context of `Implementation` smart contract from contract to library. Update their functions by removing the `payable` 
    modifier.

--- src/Implementation.sol
+++ src/Implementation.sol
@@ -6,33 +6,17 @@ pragma solidity 0.8.10;
 /// The implementation contract for the Proxy (see: `Proxy.sol`).
 ///
 /// Only deployed once and the implementation is reused by all proxy contracts.
-contract Implementation {
+library Implementation {
 
-    function callContract(address a, bytes calldata _calldata) payable external returns (bytes memory) {
+    function callContract(address a, bytes calldata _calldata) external returns (bytes memory) {
         (bool success , bytes memory ret) =  a.call{value: msg.value}(_calldata);
         require(success);
         return ret;
     }
 
-    function delegatecallContract(address a, bytes calldata _calldata) payable external returns (bytes memory) {
+    function delegatecallContract(address a, bytes calldata _calldata) external returns (bytes memory) {
         (bool success, bytes memory ret) =  a.delegatecall(_calldata);
         require(success);
         return ret;
     }
 }
-
-
-// library ImplementationV2 {
-
-//     function callContract(address a, bytes calldata _calldata) external returns (bytes memory) {
-//         (bool success , bytes memory ret) =  a.call{value: msg.value}(_calldata);
-//         require(success);
-//         return ret;
-//     }
-
-//     function delegatecallContract(address a, bytes calldata _calldata) external returns (bytes memory) {
-//         (bool success, bytes memory ret) =  a.delegatecall(_calldata);
-//         require(success);
-//         return ret;
-//     }
-// }
